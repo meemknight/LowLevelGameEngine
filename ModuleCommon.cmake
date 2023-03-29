3d78541efed3e590e9d975ab43b87e2097dde73e
meemknight <vladalex420@gmail.com>
2023-03-29 17:40:28 +0300

    imrpoved module api

---
+++ ModuleCommon.cmake
@@ -0,0 +1,7 @@
+# Define MY_SOURCES to be a list of all the source files for my game 
+file(GLOB_RECURSE MY_SOURCES CONFIGURE_DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp")
+add_library(${PROJECT_NAME})
+target_sources(${PROJECT_NAME} PRIVATE "${MY_SOURCES}")
+target_include_directories(${PROJECT_NAME} PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/include")
+target_link_libraries(${PROJECT_NAME} PUBLIC core) #link with other modules or libraries
+
