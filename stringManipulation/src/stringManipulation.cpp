8c9db350952fed28643e5d898186f44aff3b8fd5
Abhirup Bhattacharyya <abhirup27022001@outlook.com>
2023-04-02 14:54:36 +0530

    changed implementation of findChar(std::string_view, char). Replaced recursion with a loop.

--- stringManipulation/src/stringManipulation.cpp
+++ stringManipulation/src/stringManipulation.cpp
@@ -7,24 +7,20 @@ namespace LLGE
 		//when implementing this functions try to not repeat yourself
 		size_t stringManipulation::findChar(std::string_view source, char character)
 		{
-			static size_t index=0;
-			if(source.size() >= index)
-			{
 
-				if (source[index] != character)
-				{
-					index++;
-					findChar(source, character);
-				}
-				else
-				{
-					return index;
-				}
-			}
-			else
+			size_t index = 0;
+			for (; index <= source.size(); index++)
 			{
-				return (size_t)-1;
+				if (source[index] == character)
+				{
+					break;
+				}
 			}
+			if (index > source.size())
+			{
+				return -1;
+			}
+			return index;
 		}
 		void stringManipulation::toLower(std::string_view source, std::string& dest)
 		{
