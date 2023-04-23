d926c8125efa09b9ad16bf218e6842331c2381f8
noobmaster0 <bransonparley@gmail.com>
2023-04-23 11:49:54 -0600

    Added bitwise operators and fixed bug
    Added bitwise operators for vector math
    fixed bug were *= and /= would subtract

--- math/include/llm.h
+++ math/include/llm.h
@@ -7,7 +7,7 @@ namespace LLGE
     namespace LLM
     {
         template<class T>
-        struct Impl
+        struct Impl // defining operations for the Vectors.
         {
             T operator-()
             {
@@ -59,6 +59,36 @@ namespace LLGE
                 return ret;
             }
 
+            T operator&(T other)
+            {
+                auto ret = *(T*)this;
+                for (int i = 0; i < T::Size; i++)
+                {
+                    ret.data[i] &= other.data[i];
+                }
+                return ret;
+            }
+
+            T operator|(T other)
+            {
+                auto ret = *(T*)this;
+                for (int i = 0; i < T::Size; i++)
+                {
+                    ret.data[i] |= other.data[i];
+                }
+                return ret;
+            }
+
+            T operator^(T other)
+            {
+                auto ret = *(T*)this;
+                for (int i = 0; i < T::Size; i++)
+                {
+                    ret.data[i] ^= other.data[i];
+                }
+                return ret;
+            }
+
             T operator+=(T other)
             {
                 *(T*)this = *(T*)this + other;
@@ -75,14 +105,35 @@ namespace LLGE
 
             T operator/=(T other)
             {
-                *(T*)this = *(T*)this - other;
+                *(T*)this = *(T*)this / other;
 
                 return *(T*)this;
             }
 
             T operator*=(T other)
             {
-                *(T*)this = *(T*)this - other;
+                *(T*)this = *(T*)this * other;
+
+                return *(T*)this;
+            }
+
+            T operator&=(T other)
+            {
+                *(T*)this = *(T*)this & other;
+
+                return *(T*)this;
+            }
+
+            T operator|=(T other)
+            {
+                *(T*)this = *(T*)this | other;
+
+                return *(T*)this;
+            }
+
+            T operator^=(T other)
+            {
+                *(T*)this = *(T*)this ^ other;
 
                 return *(T*)this;
             }
@@ -113,9 +164,14 @@ namespace LLGE
 
         };
 
+        // Vector2 aliases
         typedef Vector2<float> vec2;
+        typedef Vector2<float> vec2f;
+        typedef Vector2<float> Vector2f;
         typedef Vector2<int> vec2i;
+        typedef Vector2<int> Vector2i;
         typedef Vector2<unsigned int> vec2u;
+        typedef Vector2<unsigned int> Vector2u;
 
         template<class T>
         struct Vector3 : public Impl<Vector3<T>>
@@ -139,8 +195,13 @@ namespace LLGE
             };
         };
 
+        // Vector3 aliases
         typedef Vector3<float> vec3;
+        typedef Vector3<float> vec3f;
+        typedef Vector3<float> Vector3f;
         typedef Vector3<int> vec3i;
+        typedef Vector3<int> Vector3i;
         typedef Vector3<unsigned int> vec3u;
+        typedef Vector3<unsigned int> Vector3u;
     };
 };
